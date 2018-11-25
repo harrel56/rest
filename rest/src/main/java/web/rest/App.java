7e134ecbf6cf306c9b704c59ff77cbc56265cd6d
harrel56 <marcin.slowik95@gmail.com>
2018-11-25 14:08:06 +0100

    CON-4 added profiles (default = prod)

--- rest/src/main/java/web/rest/App.java
+++ rest/src/main/java/web/rest/App.java
@@ -5,7 +5,6 @@ import java.util.concurrent.Executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,9 +13,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @SpringBootApplication
 @EnableAsync
 @ImportResource("beans.xml")
-@PropertySource("properties/application.properties")
-@ComponentScan("web.rest.user.management")
-@ComponentScan("web.rest.security")
+@PropertySource("properties/application-${spring.profiles.active:prod}.properties")
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
