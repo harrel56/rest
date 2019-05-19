46c310536235c09a683c7c7b221c7dcc60d4beed
Harrel <marcin.slowik95@gmail.com>
2019-05-19 23:42:52 +0200

    FE-2 users & events table

---
+++ rest/src/main/js/GenericTable.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const idField = 'id'
+
+class GenericTable extends React.Component {
+  render () {
+    return (
+        <table className='table'>
+          <thead className='thead-dark'>
+            <tr>
+              {this.props.fields.map(header =>
+                <th scope='col' key={header}>{header}</th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.data.map(row =>
+                <tr key={row[idField]}>
+                    {
+                        this.props.fields.map(field =>
+                        <td key={field}>{row[field]}</td>
+                    )}
+                </tr>
+            )}
+          </tbody>
+        </table>
+    )
+  }
+}
+
+export default GenericTable
